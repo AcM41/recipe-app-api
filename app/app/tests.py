06a065fc46fd0cbebd9109fb01d5aca9c872d137
ZM <meng0051@algonquinlive.com>
2023-09-26 16:19:09 -0400

    test_command

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,25 +5,26 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
     def test_add(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract(self):
         """Test subtract numbers."""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
 
     def test_add_and_sub(self):
         """Test add and subtract function together"""
-        res1 = calc.add(1,2)
-        res2 = calc.subtract(1,2)
+        res1 = calc.add(1, 2)
+        res2 = calc.subtract(1, 2)
 
-        self.assertEqual(res1,3)
-        self.assertEqual(res2,1)
+        self.assertEqual(res1, 3)
+        self.assertEqual(res2, 1)
